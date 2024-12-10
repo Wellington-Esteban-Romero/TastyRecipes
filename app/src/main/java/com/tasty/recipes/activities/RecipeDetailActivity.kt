3311b62ccf4180cef52a82d1f570c898960d04c0
Wellington <wellington.esteban.romero@gmail.com>
2024-12-11 00:58:02 +0100

    change color icon and add method toolBar

--- app/src/main/java/com/tasty/recipes/activities/RecipeDetailActivity.kt
+++ app/src/main/java/com/tasty/recipes/activities/RecipeDetailActivity.kt
@@ -1,10 +1,12 @@
 package com.tasty.recipes.activities
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 import com.squareup.picasso.Picasso
 import com.tasty.recipes.R
 import com.tasty.recipes.data.entities.Recipe
@@ -17,6 +19,7 @@ class RecipeDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRecipeDetailBinding
     private lateinit var recipeDAO: RecipeDAO
     private lateinit var recipe:Recipe
+    private lateinit var toolbar:MaterialToolbar
 
     companion object {
         val EXTRA_RECIPE_ID = "EXTRA_RECIPE_ID"
@@ -33,7 +36,9 @@ class RecipeDetailActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         init()
+        initListener()
     }
 
     private fun init () {
@@ -41,11 +46,32 @@ class RecipeDetailActivity : AppCompatActivity() {
 
         recipeDAO = RecipeDAO(this)
 
+        toolbar = findViewById(R.id.toolbar)
         recipe = recipeDAO.findRecipeById(id)!!
 
         createDetails(recipe)
 
         println(id)
+    }
+
+    private fun initListener () {
+
+        toolbar.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+        toolbar.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.action_favorite -> {
+                    Toast.makeText(this, "Added to Favorites", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.action_share -> {
+                    Toast.makeText(this, "Sharing Recipe...", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     private fun createDetails (recipe: Recipe) {

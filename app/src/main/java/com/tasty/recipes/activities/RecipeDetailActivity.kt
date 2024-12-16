4428d297f09375ca8a9e1f8465f1b32860581bfa
Wellington <wellington.esteban.romero@gmail.com>
2024-12-16 13:16:58 +0100

    toolbar

--- app/src/main/java/com/tasty/recipes/activities/RecipeDetailActivity.kt
+++ app/src/main/java/com/tasty/recipes/activities/RecipeDetailActivity.kt
@@ -1,6 +1,8 @@
 package com.tasty.recipes.activities
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,15 +13,18 @@ import com.tasty.recipes.R
 import com.tasty.recipes.data.entities.Recipe
 import com.tasty.recipes.data.providers.RecipeDAO
 import com.tasty.recipes.databinding.ActivityRecipeDetailBinding
+import com.tasty.recipes.utils.SessionManager
 
 class RecipeDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRecipeDetailBinding
     private lateinit var recipeDAO: RecipeDAO
     private lateinit var recipe:Recipe
+    private lateinit var idRecipe: String
 
     companion object {
         val EXTRA_RECIPE_ID = "EXTRA_RECIPE_ID"
+        lateinit var session: SessionManager
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +32,8 @@ class RecipeDetailActivity : AppCompatActivity() {
 
         binding = ActivityRecipeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -38,15 +45,18 @@ class RecipeDetailActivity : AppCompatActivity() {
     }
 
     private fun initUI () {
-        val id = intent.getStringExtra(EXTRA_RECIPE_ID).orEmpty()
+
+        session = SessionManager(applicationContext)
+
+        idRecipe = intent.getStringExtra(EXTRA_RECIPE_ID).orEmpty()
 
         recipeDAO = RecipeDAO(this)
 
-        recipe = recipeDAO.findRecipeById(id)!!
+        recipe = recipeDAO.findRecipeById(idRecipe)!!
 
         createDetails(recipe)
 
-        println(id)
+        println(idRecipe)
     }
 
     private fun initListener () {
@@ -54,19 +64,37 @@ class RecipeDetailActivity : AppCompatActivity() {
         binding.toolbar.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
-        binding.toolbar.setOnMenuItemClickListener { item ->
-            when (item.itemId) {
-                R.id.action_favorite -> {
-                    Toast.makeText(this, "Added to Favorites", Toast.LENGTH_SHORT).show()
-                    true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.recipe_toolbar_menu, menu)
+        if (session.isFavorite(idRecipe))
+            menu?.findItem(R.id.actionFavorite)?.setIcon(R.drawable.ic_favorite)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        println(item.icon)
+        when (item.itemId) {
+            R.id.action_favorite -> {
+                Toast.makeText(this, "Added to Favorites", Toast.LENGTH_SHORT).show()
+                if (!session.isFavorite(idRecipe)) {
+                    session.saveRecipe(idRecipe, SessionManager.ACTIVE)
+                    item.setIcon(R.drawable.ic_favorite)
+                } else {
+                    session.saveRecipe(idRecipe, SessionManager.DES_ACTIVE)
+                    item.setIcon(R.drawable.ic_favorite_empty)
                 }
-                R.id.action_share -> {
-                    Toast.makeText(this, "Sharing Recipe...", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                else -> false
+                true
             }
+            R.id.action_share -> {
+                Toast.makeText(this, "Sharing Recipe...", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> false
         }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun createDetails (recipe: Recipe) {

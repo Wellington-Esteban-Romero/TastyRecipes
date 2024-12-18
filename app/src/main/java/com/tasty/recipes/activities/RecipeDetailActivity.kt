c7aa2a74253e7c61ed59eeba96d13265bdf66c24
Tardes <wellington.esteban.romero@gmail.com>
2024-12-18 21:01:05 +0100

    delete for show behind image detail

--- app/src/main/java/com/tasty/recipes/activities/RecipeDetailActivity.kt
+++ app/src/main/java/com/tasty/recipes/activities/RecipeDetailActivity.kt
@@ -42,11 +42,6 @@ class RecipeDetailActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
         initUI()
         initListener()
     }
